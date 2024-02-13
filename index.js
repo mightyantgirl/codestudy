93113e609d9eea776445eab7bcfc3ef4ade63b6b
mightyantgirl <155346084+mightyantgirl@users.noreply.github.com>
2024-02-13 23:53:25 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,65 @@
 
-
 let body = document.querySelector('body');
 let h1 = document.querySelector('h1');
 let input = document.querySelector('input');
 
-function night_button(){
-  if(this.value === '야간모드'){
+function night_button(button){
+  if(button.value === '야간모드'){
     body.style.backgroundColor = '#1e1e1e';
     h1.style.color = '#f3f3f3';
-    this.value = '주간모드';
-    
+    button.style.backgroundColor = '#242424';
+    button.style.color = '#a7a7a7';
+    button.value = '주간모드';
+
+
     let menu = document.querySelectorAll('.menu');
-    let i = 0;
-    while(1 < menu.length){
-        menu[i].style.backgroundColor = '#242424';
-        menu[i].style.color = '#fff';
-        i = i + 1;
+    for (let i = 0; i < menu.length; i++){
+      menu[i].style.backgroundColor = '#242424';
+      menu[i].style.color = '#f3f3f3';
     }
-    } else {
+    
+    let main = document.querySelectorAll('.main');
+    for (let i = 0; i < main.length; i++){
+      main[i].style.color = '#f3f3f3';
+    }
+
+    let alist = document.querySelectorAll('a');
+    for (let i = 0; i < alist.length; i++){
+      alist[i].style.color = '#a7a7a7';
+    }
+
+    let subtitle = document.querySelectorAll('.sub_title_style');
+    for (let i = 0; i < subtitle.length; i++){
+      subtitle[i].style.color = '#f3f3f3';
+    }
+
+  } else {
     body.style.backgroundColor = '#f3f3f3';
-    body.style.color = '#000000';
     h1.style.color = '#3d3d3d';
-    this.value = '야간모드';
-    
+    button.style.backgroundColor = '#e9e9e9';
+    button.style.color = '#b1b1b1';
+    button.value = '야간모드';
+
+
     let menu = document.querySelectorAll('.menu');
-    let i = 0;
-    while(1 < menu.length){
-        menu[i].style.backgroundColor = '#fff';
-        menu[i].style.color = '#242424';
-        i = i + 1;
+    for (let i = 0; i < menu.length; i++){
+      menu[i].style.backgroundColor = '#fff';
+      menu[i].style.color = '#242424';
     }
+
+    let main = document.querySelectorAll('.main');
+    for (let i = 0; i < main.length; i++){
+      main[i].style.color = '#242424';
     }
-}
+
+    let alist = document.querySelectorAll('a');
+    for (let i = 0; i < alist.length; i++){
+      alist[i].style.color = '#3d3d3d';
+    }
+
+    let subtitle = document.querySelectorAll('.sub_title_style');
+    for (let i = 0; i < subtitle.length; i++){
+      subtitle[i].style.color = '#242424';
+    }
+  }
+};
